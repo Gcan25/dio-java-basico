2c1fb05c7e61237164e3e291eadaae089912cf70
Gcan25 <canariogabriel18@gmail.com>
2024-06-10 17:11:28 -0300

    Variaveis

--- meu-primeiro-projeto-java/src/edu/gabriel/primeirasemana/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/edu/gabriel/primeirasemana/TiposVariaveis.java
@@ -2,5 +2,14 @@ package edu.gabriel.primeirasemana;
 
 
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2.500;
+
+        System.out.println(salarioMinimo);
+        // A marcação 'final' garante que a variavel não seja alterada.
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
+    }
     
 }
