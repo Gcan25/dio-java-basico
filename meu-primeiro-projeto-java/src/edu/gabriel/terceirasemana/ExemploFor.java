2e414868bec79b84eb1fdfb9afa2011576962660
Gcan25 <canariogabriel18@gmail.com>
2024-06-20 18:34:18 -0300

    exeções

---
+++ meu-primeiro-projeto-java/src/edu/gabriel/terceirasemana/ExemploFor.java
@@ -0,0 +1,9 @@
+package edu.gabriel.terceirasemana;
+
+public class ExemploFor {
+	public static void main(String[] args) {
+		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		}
+	}
+}
